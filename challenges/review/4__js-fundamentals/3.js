c9dad3f531eded9aac4466cf9b48c18ddc526d45
Mandy <34908590+picaq@users.noreply.github.com>
2021-02-09 18:49:37 -0800

    complete review-4 3.js with Array methods and console.log

--- challenges/review/4__js-fundamentals/3.js
+++ challenges/review/4__js-fundamentals/3.js
@@ -5,3 +5,19 @@
 //   - Add a new item to the end of the array (ex: [1,2,3,4])
 //   - Add an array to the beginning of the array. (ex: [["a","b","c"],1,2,3,4])
 //   - Return the first item from the first item in the array. (ex: "a")
+
+let numArr = [1, 2, 3, 4, 5, 6];
+console.log({numArr});
+let first = numArr[0];
+console.log({first});
+let last = numArr[5];
+console.log({last});
+numArr.push("new item");
+console.log("push new item to end", numArr);
+numArr.unshift(["a", "b", "c", "d", "e", "f"]);
+console.log("add array to beginning", numArr);
+let firstFirst = numArr[0][0];
+console.log({firstFirst});
+
+
+

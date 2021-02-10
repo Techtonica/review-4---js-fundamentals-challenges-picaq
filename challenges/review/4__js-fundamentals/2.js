a143a5b742b2a6cba63e2d0091689baed1a5b170
Mandy <34908590+picaq@users.noreply.github.com>
2021-02-09 18:11:38 -0800

    complete review-4 2.js with str.length

--- challenges/review/4__js-fundamentals/2.js
+++ challenges/review/4__js-fundamentals/2.js
@@ -1,3 +1,37 @@
 // Write a function that takes a string as the argument.
 // The function should return a number that is the number of characters in the string.
 // Choose names that make the code easy to understand.
+
+let numChars = function(str) {
+    return str.length;
+}
+
+let testcases = [
+    "apple", "banana", "celery", "cheese", "I love cows"
+]
+let expected = [
+    5, 6, 6, 6, 11
+]
+
+let prtTitle = function(num) { console.log(`
+------ Challenge ${num} ------`);
+}
+
+let testMe = function(num, funct, testcases, expected, option = 1) {
+	prtTitle(num);
+	for ( let test of testcases ) {
+		let expect;
+		if ( funct(test) === expected[testcases.indexOf(test)] ) {
+			expect = ["✓"];
+		} else {
+			expect = ["expect:", expected[testcases.indexOf(test)]];
+		}
+		if ( option == 0 ) {
+			console.log(...expect);
+		} else {
+			console.log(funct(test), ...expect);
+		}
+	}
+}
+
+testMe(2, numChars, testcases, expected);
